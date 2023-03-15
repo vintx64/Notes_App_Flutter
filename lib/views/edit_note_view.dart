4ba37b31ae019f220c6c7b200f5413dda5011839
vintx64 <vintx64@gmail.com>
2023-03-15 23:25:00 +0200

    create edit note view

---
+++ lib/views/edit_note_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:notes_app_flutter/views/widgets/edit_note_view_body.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: EditNoteViewBody(),
+    );
+  }
+}
--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,52 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app_flutter/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-        ListTile(
-          title: const Text(
-            'Flutter Tips',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 26,
-            ),
-          ),
-          subtitle: Padding(
-            padding: const EdgeInsets.only(top: 16, bottom: 16),
-            child: Text(
-              'Tempor velit laborum cupidatat culpa .',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
               style: TextStyle(
-                color: Colors.black.withOpacity(.5),
-                fontSize: 18,
+                color: Colors.black,
+                fontSize: 26,
               ),
             ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 16),
+              child: Text(
+                'Tempor velit laborum cupidatat culpa .',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.5),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                )),
           ),
-          trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
-              )),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(right: 24),
-          child: Text(
-            'May 21 , 2022',
-            style: TextStyle(color: Colors.black.withOpacity(.4), fontSize: 16),
-          ),
-        )
-      ]),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              'May 21 , 2022',
+              style:
+                  TextStyle(color: Colors.black.withOpacity(.4), fontSize: 16),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
